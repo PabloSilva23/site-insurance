31562f1923773726314f9a51524e0ec0ab5d6528
Pablo Silva <pablosilvax69@gmail.com>
2022-03-19 19:26:00 -0300

    add scrollReval

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const bars = document.getElementById('bar');
 const menu = document.querySelector('.menu');
+const logo = document.getElementById('logo');
+
 
 bars.addEventListener('click', () => {
     bars.classList.toggle('fa-times');
     menu.classList.toggle('show');
-
     if (bars.classList.contains('fa-times')) {
         bars.style.color = '#737373';
     }
@@ -13,7 +14,32 @@ bars.addEventListener('click', () => {
     };
 });
 
-const logo = document.getElementById('logo');
+const links = document.querySelectorAll('nav ul li');
 
 
+for (let link of links) {
+    link.addEventListener('click', function () {
+        menu.classList.remove('show');
+    })
+};
 
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    if(this.window.scrollY > header.scrollHeight) {
+        bars.style.color = 'black';
+    }
+    if(this.window.scrollX > 900) {
+        this.document.querySelector('input').style.backgroundColor = 'red';
+    }
+    else {
+        bars.style.color = 'white';
+    }
+});
+
+// ScrollReveall
+
+ScrollReveal().reveal('.container .about-input', {duration: 1000, distance: '100px', position: 'bottom'});
+
+ScrollReveal().reveal('.card-review', {duration: 2000, distance: '100px', position: 'bottom'});
+ScrollReveal().reveal('.container', {duration: 2000, distance: '100px', position: 'bottom'});
